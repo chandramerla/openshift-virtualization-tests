1d1c357a130c66e4e20606e7b0cca7109d6036d0
Kirk Crane <kcrane@redhat.com>
2025-04-22 21:28:42 -0400

    update to use ArchImages

--- tests/global_config_s390x.py
+++ tests/global_config_s390x.py
@@ -13,32 +13,7 @@ from utilities.constants import (
 global config
 global_config = pytest_testconfig.load_python(py_file="tests/global_config.py", encoding="utf-8")
 
-
-# No support for cirros on s390x.  Use Fedora instead
-class Cirros(Images.Cirros):
-    RAW_IMG = "Fedora-Cloud-Base-Generic-41-1.4.s390x.raw"
-    RAW_IMG_GZ = "Fedora-Cloud-Base-Generic-41-1.4.s390x.raw.gz"
-    RAW_IMG_XZ = "Fedora-Cloud-Base-Generic-41-1.4.s390x.raw.xz"
-    QCOW2_IMG = "Fedora-Cloud-Base-Generic-41-1.4.s390x.qcow2"
-    QCOW2_IMG_GZ = "Fedora-Cloud-Base-Generic-41-1.4.s390x.qcow2.gz"
-    QCOW2_IMG_XZ = "Fedora-Cloud-Base-Generic-41-1.4.s390x.qcow2.xz"
-    DISK_DEMO = "fedora-cloud-registry-disk-demo"
-    DIR = "cnv-tests/fedora-images"
-    DEFAULT_DV_SIZE = "10Gi"
-    DEFAULT_MEMORY_SIZE = "1Gi"
-
-
-class Cdi(Images.Cdi):
-    QCOW2_IMG = "Fedora-Cloud-Base-Generic-41-1.4.s390x.qcow2"
-    DIR = "cnv-tests/fedora-images"
-    DEFAULT_DV_SIZE = "10Gi"
-
-
-Images.Cirros = Cirros
 utilities.constants.OS_FLAVOR_CIRROS = "fedora"
-Images.Cdi = Cdi
-Images.Fedora.FEDORA41_IMG = "Fedora-Cloud-Base-Generic-41-1.4.s390x.qcow2"
-Images.Rhel.RHEL9_5_IMG = "rhel-95-s390x.qcow2"
 EXPECTED_CLUSTER_INSTANCE_TYPE_LABELS[PREFERENCE_STR] = f"rhel.9.{S390X}"
 
 for _dir in dir():
