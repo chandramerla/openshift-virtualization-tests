ae40dc0b2dcd951817bd95142239d27be8ea871e
Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>
2025-08-03 18:26:45 +0530

    Add s390x marker for the initial tier2 storage tests.
    Signed-off-by: Vamsi Krishna Siddu <vamsikrishna.siddu@ibm.com>

--- tests/storage/cdi_clone/test_clone.py
+++ tests/storage/cdi_clone/test_clone.py
@@ -207,6 +207,7 @@ def test_successful_vm_from_cloned_dv_windows(
     ],
     indirect=True,
 )
+@pytest.mark.s390x
 def test_disk_image_after_clone(
     skip_block_volumemode_scope_function,
     unprivileged_client,
@@ -286,6 +287,7 @@ def test_successful_snapshot_clone(
 
 @pytest.mark.gating
 @pytest.mark.polarion("CNV-5607")
+@pytest.mark.s390x
 def test_clone_from_fs_to_block_using_dv_template(
     skip_test_if_no_filesystem_sc,
     skip_test_if_no_block_sc,
@@ -307,6 +309,7 @@ def test_clone_from_fs_to_block_using_dv_template(
 
 @pytest.mark.polarion("CNV-5608")
 @pytest.mark.smoke()
+@pytest.mark.s390x
 def test_clone_from_block_to_fs_using_dv_template(
     skip_test_if_no_filesystem_sc,
     skip_test_if_no_block_sc,
